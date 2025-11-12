6f6b5fe285770164e851e55aa68575b62622c569
FaresBza <fares.bouzaria@crea-inseec.com>
2025-11-12 17:53:17 +0100

    feat(routes): Adding routes for new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ import { DevProductsComponent } from './dev/dev-products.component';
 import { DevProductRatingComponent } from './dev/dev-product-rating.component';
 import { AppPlaceholderComponent } from './app-placeholder.component';
 
+import { Login } from './login/login';
+import { Products } from './products/products';
+import { Rating } from './rating/rating';
+
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'dev', component: DevIndexComponent },
@@ -13,5 +17,8 @@ export const routes: Routes = [
   { path: 'dev/products', component: DevProductsComponent },
   { path: 'dev/products/:id/rating', component: DevProductRatingComponent },
   { path: 'app', component: AppPlaceholderComponent },
+  { path: '/login', component: Login },
+  { path: '/shop/products', component: Rating },
   { path: '**', redirectTo: '' },
+  
 ];
