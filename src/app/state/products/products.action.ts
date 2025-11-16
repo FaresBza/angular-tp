8aede5836e562d43feb4b57ccef1e6c517190f57
FaresBza <fares.bouzaria@crea-inseec.com>
2025-11-16 17:19:19 +0100

    feat(products): Added NgRx actions for product selection and product rating

--- src/app/state/products/products.action.ts
+++ src/app/state/products/products.action.ts
@@ -11,5 +11,8 @@ export const ProductsActions = createActionGroup({
         'loadProducts': props<{ page?: number, pageSize?: number, minRating?: number, ordering?: string }>(),
         'loadProductsSuccess': props<{ data: ProductsSucessResponse }>(),
         'loadProductsFailure': props<{ error: string }>(),
+        'selectProduct': props<{ id: number }>(),
+        'loadRating': props<{ id: number }>(),
+        'loadRatingSuccess': props<{ product_id: number; avg_rating: number; count: number }>(),
     },
 });
