4ef984553916d973ea2663651ec506aea5f0f3a7
FaresBza <fares.bouzaria@crea-inseec.com>
2025-11-16 22:56:50 +0100

    feat(products): bind data source to product table in template

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
+
+import { ProductsActions } from '../state/products/products.action';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { selectProductsList } from '../state/products/products.selectors';
 
 @Component({
   selector: 'app-products',
-  imports: [RouterLink],
+  standalone: true,
   templateUrl: './products.html',
-  styleUrl: './products.css',
+  styleUrls: ['./products.css'],
+  imports: [
+    AsyncPipe,
+    MatCardModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+  ],
 })
-export class Products {
+export class Products implements OnInit {
+  private store = inject(Store);
 
+  displayedColumns = ['id', 'name', 'price', 'created_at', 'avg'];
+
+  products$ = this.store.select(selectProductsList);
+
+  ngOnInit(): void {
+    this.store.dispatch(
+      ProductsActions.loadProducts({
+        page: 1,
+        pageSize: 20,
+        minRating: 0,
+        ordering: '-created_at',
+      })
+    );
+  }
 }
