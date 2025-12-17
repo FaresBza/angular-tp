9a35c978e37e412a1c3f4e3e2a55373e47609d6c
FaresBza <fares.bouzaria@crea-inseec.com>
2025-12-17 14:42:44 +0100

    tests(login): Add unit tests for login component

--- src/app/login/login.spec.ts
+++ src/app/login/login.spec.ts
@@ -1,23 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LoginPageComponent } from './login';
+import { AuthActions } from '../state/auth/auth.actions';
 
-describe('Login', () => {
+describe('LoginPageComponent (simple)', () => {
+  let store: MockStore;
   let component: LoginPageComponent;
-  let fixture: ComponentFixture<LoginPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginPageComponent]
-    })
-    .compileComponents();
+      imports: [LoginPageComponent, RouterTestingModule, NoopAnimationsModule],
+      providers: [provideMockStore()],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(LoginPageComponent);
+    store = TestBed.inject(MockStore);
+    spyOn(store, 'dispatch');
+
+    const fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should dispatches login action on submit when form is valid', () => {
+    component.loginForm.setValue({ username: 'u', password: 'p' });
+    expect(component.loginForm.valid).toBeTrue();
+
+    component.onSubmit();
+
+    expect(store.dispatch).toHaveBeenCalledWith(
+      AuthActions.login({ username: 'u', password: 'p' }),
+    );
+  });
+
+  it('should does not dispatch when form is invalid', () => {
+    component.loginForm.setValue({ username: '', password: '' });
+    expect(component.loginForm.invalid).toBeTrue();
+
+    component.onSubmit();
+
+    expect(store.dispatch).not.toHaveBeenCalled();
   });
 });
